5e82bf88995e66f2cac6ca32fa02e460309db326
devlog98 <dno1998@hotmail.com>
2021-12-23 03:51:24 -0300

    [ADD] Transaction Type seed to generate options

--- leitor-cnab/db/seeds.rb
+++ leitor-cnab/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+#Creating all Transaction Types of the application
+TransactionType.create(identifier: 1, name: "Débito", operation: 1)
+TransactionType.create(identifier: 2, name: "Boleto", operation: 0)
+TransactionType.create(identifier: 3, name: "Financiamento", operation: 0)
+TransactionType.create(identifier: 4, name: "Crédito", operation: 1)
+TransactionType.create(identifier: 5, name: "Recebimento Empréstimo", operation: 1)
+TransactionType.create(identifier: 6, name: "Vendas", operation: 1)
+TransactionType.create(identifier: 7, name: "Recebimento TED", operation: 1)
+TransactionType.create(identifier: 8, name: "Recebimento DOC", operation: 1)
+TransactionType.create(identifier: 9, name: "Aluguel", operation: 0)
