0305ab2cc8987027431f4bf8d8e58e23a5b97f7b
devlog98 <dno1998@hotmail.com>
2021-12-23 03:55:08 -0300

    [MOD] Removing routes from Transaction Type

--- leitor-cnab/config/routes.rb
+++ leitor-cnab/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :transactions, :except => [:edit, :update, :show]
   resources :stores
-  resources :transaction_types
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "transactions#index"
